8d1d7f2464d8d8b5998b923c7fe59e1d44b71db2
cjf_glb <cjf_glb@qq.com>
2021-11-27 11:47:39 +0800

    添加LeaveController

---
+++ src/main/java/com/xdlr/infosys/controller/LeaveController.java
@@ -0,0 +1,25 @@
+package com.xdlr.infosys.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+
+@RestController(value = "api/v0")
+public class LeaveController {
+    @RequestMapping(value = "/leave")
+    public ModelAndView leaveRequest(){return new ModelAndView("ask_for_leave");}
+    @RequestMapping(value = "/leave/apply/{leaveType}/{startDate}/{startTime}/{endDate}/{endTime}/{reason}")
+    public ModelAndView leaveApply(@PathVariable(value = "leaveType")String leaveType,
+                                   @PathVariable(value = "startData") Date startDate,
+                                   @PathVariable(value = "startTime")String startTime,
+                                   @PathVariable(value = "endData")Date endData,
+                                   @PathVariable(value = "endTime")String endTime,
+                                   @PathVariable(value = "reason")String reason){
+        if()
+        return  null;
+    }
+}
+
