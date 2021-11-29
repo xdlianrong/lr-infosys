f896ec9861398a1439011f96aaac97ab1959ed3d
cjf_glb <cjf_glb@qq.com>
2021-11-29 09:36:11 +0800

    change LeaveController

--- src/main/java/com/xdlr/infosys/controller/LeaveController.java
+++ src/main/java/com/xdlr/infosys/controller/LeaveController.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Date;
 
-@RestController(value = "/api/v0")
+@RestController
 public class LeaveController {
     @RequestMapping(value = "/leave")
     public ModelAndView leaveRequest(){return new ModelAndView("ask_for_leave");}
@@ -18,10 +18,6 @@ public class LeaveController {
                                    @PathVariable(value = "endData")Date endData,
                                    @PathVariable(value = "endTime")String endTime,
                                    @PathVariable(value = "reason")String reason){
-        /**
-         * TODO
-         */
-
         return  null;
     }
 }
